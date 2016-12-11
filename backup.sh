3765be7386c5b8be3428ae53bb635e2121ee9ac2
root <root@vishal.com>
2016-12-10 23:19:04 -0800

    backup script with trap

---
+++ backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Sat Dec 10 22:51:00 PST 2016
+#author vishal patil#
+#backup purpose#
+
+ddate=`date +%F`
+logs=/var/log/backup.log
+lock_file=/tmp/$0.lock
+
+read -p "Enter path for backup eg. /var/www :-" backup_path
+read -p "Enter what name you have to use for backu eg. abc or apache " backup_name
+
+trap '{rm -rf $lock_file; exit 1;}' INT TERM
+
+cleanup () {
+echo "cleanup process is started" >> $logs
+rm -rf $lock_file
+echo "cleanup process is complited" >> $logs
+}
+
+backup () {
+echo " backup proccess is going to start" >> $logs
+tar -zcvf /tmp/$backup_name-$ddate.tgz  $backup_path >> $logs
+echo "backup proccess is comlited" >> $logs
+}
+
+if [[ ! -f $lock_file ]]; then 
+	backup
+	   if [[ $? -eq 0 ]];then
+          	cleanup
+	   else 
+		echo "backup is fialed" >> $logs
+		cleanup
+	   fi
+else
+	echo "backup is already running" >> $logs
+fi
+
