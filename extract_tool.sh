9f3c8d398030d7f77a78971db90fcf38b2fea6b8
patil-vishal <root@vishal.com>
2017-04-13 11:21:01 +0530

    extract tool with whiptail

--- extract_tool.sh
+++ extract_tool.sh
@@ -7,53 +7,49 @@
 #		Warning : Kindly don't change below code						#
 #########################################################################################################
 
+whiptail --title "Extract tool" --msgbox "Welcome to extract tool. Press enter to continue"  10 60
+while true ; do
+#whiptail --title "Extract tool" --msgbox "Welcome to extract tool. Press enter to continue"  10 60
 
-tar_extract () {
-tar -xvf $fname
-}
-
-tar_gz () {
-tar -zxvf $fname
-}
-
-gz_file () {
-gunzip $fname
-}
-
-bz_file () {
-bunzip2 $fname
-}
-
-zip_file () {
-unzip $fname 
-}
-
-read -p " Enter your file name or " fname
-
-if [[ -z $fname ]]; then
-echo "Enter valid file name " && exit
+ftype=`whiptail --title "Compression Type" --radiolist "Choose compression type" 20 50 5 "tar" "to extract tar file" ON "tgz" "to extract tar.gz file" OFF "zip" "to extract zip file" OFF "gzip" "to extract gzip file" OFF "bzip2" "to extract bzip2 file" OFF 3>&1 1>&2 2>&3`
+if [[ $? -ne 0 ]]; then
+	echo "Thanks for using extract tool" && break 
 else 
-	ls $fname | egrep '{.tar|.bz2|.gz|.tgz|.zip}'  &> /dev/null
-	if [[ $? -eq 0 ]]; then
-	case $fname in
-	
-		*.tgz)
-			tar_gz
-			;;
-		*.tar)
-			tar_extract
-			;;
-		*.gz)
-			gz_file
-			;;
-		*.bz2)
-			bz_file
-			;;
-		*.zip) 
-			zip_file
-			;;
-	esac
-	else 
-	echo "Please enter valid file with valid format/extention eg. .tar,.bz2,.zip,.gz,.tgz"
+fname=`whiptail --inputbox "Enter file name" 8 78 "eg. abc.tgz" --title "File Name" 3>&1 1>&2 2>&3`
+	if [[ $? -ne 0  ]]; then
+	 echo "Thanks for using extract tool" && break
+	else
+	if [[ -z $fname ]]; then
+	whiptail --title "Warning!" --msgbox "There is some problem in file name/type. Please enter valid file $ftype type"  10 60 && continue
+	else
+	fname1=`ls $fname | awk -F. '{ print $NF }'` 
+	fi
+if [[ $ftype = $fname1 ]]; then 
+			case $fname in
+			
+				*.tgz)
+					tar -zxvf  $fname
+					;;
+				*.tgz.gz)
+					tar -zxvf  $fname
+					;;
+				*.tar)
+					tar -xvf  $fname
+					;;
+				*.gz)
+					gunzip $fname
+					;;
+				*.bz2)
+					bunzip2 $fname
+					;;
+				*.zip) 
+					unzip  $fname
+					;;
+			esac
+			break
+else
+     whiptail --title "Warning!" --msgbox "There is some problem in file name/type. Please enter valid file $ftype type"  10 60 &&  continue
+		fi
 	fi
 fi
+done
